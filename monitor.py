31f31a68eb924e5d42627a135e344f41c5a9c0e2
erwinrojas <erwin.rojas@outlook.es>
2026-02-17 23:06:30 -0400

    Agregar bucle while para captura de datos

--- monitor.py
+++ monitor.py
@@ -1,2 +1,15 @@
 #monitor.py
 print("---Monitor de Sistema V1.0---")
+reading=[]
+
+while True:
+    user_input=input("Ingresa valor del sensor o pulsa Q para salir: ")
+    if user_input.lower()=='q':
+        break
+    #Convertir a Float y guardar
+    try:
+        value=float(user_input)
+        reading.append(value)
+    except ValueError:
+        print("Error> Por favor ingresa un numero valido")
+print(f"Datos capturados:{reading}")
